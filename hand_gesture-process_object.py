21127a9c4e57c502ad343f77ebe3fca6d56d4d6b
yogeshwari-vs <vsyogeshwari0703@gmail.com>
2021-06-26 18:55:35 +0530

    multiprocessing code restructured 2

--- hand_gesture-process_object.py
+++
@@ -1,7 +0,0 @@
-
-import multiprocessing
-from module.gesture_control import main_avm
-
-queue_shared = multiprocessing.Queue()
-
-process_object = multiprocessing.Process(target = main_avm, args = (queue_shared,))
