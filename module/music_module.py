40c686d3498796f51f97107c04c5804d4341a4a9
Aarthi160802 <aarthithiruvasagam@gmail.com>
2021-06-26 20:23:00 +0530

    Added end music

--- module/music_module.py
+++ module/music_module.py
@@ -7,6 +7,8 @@ pygame.init()
 sound_bird = pygame.mixer.Sound(os.path.join('Utils\Music\BirdSound.wav'))
 sound_coins = pygame.mixer.Sound(os.path.join('Utils\Music\CoinsSound.wav'))
 sound_collided = pygame.mixer.Sound(os.path.join('Utils\Music\CollidedSound.wav'))
+sound_aftercollided = pygame.mixer.Sound(os.path.join('Utils\Music\AfterCollidedSound.wav'))
+
 
 sound_button_click = pygame.mixer.Sound(os.path.join(r'Utils\Music\button_click_soundeffect.wav'))
 sound_button_enlarge = pygame.mixer.Sound(os.path.join(r'Utils\Music\button_enlarge_soundeffect.wav'))
