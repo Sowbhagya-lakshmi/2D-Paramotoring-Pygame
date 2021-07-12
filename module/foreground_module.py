ce3d2af6b8b15e005e1270b02e779ee83c5c68b9
Sowbhagya-lakshmi <sowbhagyalakshmi108@gmail.com>
2021-07-12 16:09:19 +0530

    Fixed glitch in foreground

--- module/foreground_module.py
+++ module/foreground_module.py
@@ -7,21 +7,24 @@ import global_config
 ground = pygame.image.load(os.path.join('Utils/Pics/Foreground','ground.png'))
 ground_x = 0
 ground_y = global_config.window_height-ground.get_height() 
-ground_width = ground.get_width() - 5	# To prevent glitches in background movement...WIP
+ground_width = ground.get_width() - 10	# Treating the image to be shorter in width by 10 pixels to prevent glitches
 foreground_speed = 6 	# Foreground shifts by 6 pixels in each game loop
 
 def draw_fg(win):
+	"""
+	Draws the foreground onto the screen. The scalar 10 is used to treat the image to be shorter in width by 10 pixels to prevent glitches.
+	"""
 	global ground_x, ground_width
 	# Drawing ground
-	win.blit(ground, (ground_x, ground_y))
-	win.blit(ground, (ground_width,ground_y))	
+	win.blit(ground, (ground_x+10, ground_y))
+	win.blit(ground, (ground_width+10,ground_y))	
 
 	# Ground movement
 	ground_x -= foreground_speed
 	ground_width -= foreground_speed
 	
 	# For repetition of ground movement
-	if ground_x < (ground.get_width()-5) * -1:
-		ground_x = (ground.get_width()-5)
-	if ground_width < (ground.get_width()-5) * -1:
-		ground_width = (ground.get_width()-5)
+	if ground_x < (ground.get_width() - 10) * -1:
+		ground_x = (ground.get_width() - 10)
+	if ground_width < (ground.get_width() - 10) * -1:
+		ground_width = (ground.get_width() - 10)
