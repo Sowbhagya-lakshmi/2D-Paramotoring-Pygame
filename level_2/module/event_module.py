e0dfde451bba6ed085a211801676bc11f39856b2
Sowbhagya-lakshmi <sowbhagyalakshmi108@gmail.com>
2021-07-17 23:05:16 +0530

    Fixed event loop in Level 2

--- level_2/module/event_module.py
+++ level_2/module/event_module.py
@@ -1,6 +1,8 @@
 import pygame
 import sys
 import time
+
+import global_config
 from level_2.module import bird_module
 from level_2.module import dynamic_obstacle_olaf
 from level_2.module import dynamic_obstacle_santa
@@ -12,27 +14,14 @@ from level_2.mp import process_object
 
 
 right_click = False
+total_number_of_frames = global_config.speed*global_config.game_duration
 
 def setting_up_events():
-	# Generate coin once in every 1 seconds
-	pygame.time.set_timer(pygame.USEREVENT+1, 700)
-	# Generate tree obstacles once in every 8 seconds
-	pygame.time.set_timer(pygame.USEREVENT+2, 6000)
-	# Generate rock and bush obstacles once in every 20 seconds
-	pygame.time.set_timer(pygame.USEREVENT+3, 8000)
-	# Generate bird obstacle once in every 8 seconds
-	pygame.time.set_timer(pygame.USEREVENT+4, 8000)
-	#generate reindeer obstacles
-	pygame.time.set_timer(pygame.USEREVENT+5, 20000)
-	#generate santa obstacles
-	pygame.time.set_timer(pygame.USEREVENT+6, 28000)
-	#generate giftbox obstacles
-	pygame.time.set_timer(pygame.USEREVENT+7, 40000)
 
 	pygame.event.set_blocked(None)
-	pygame.event.set_allowed([pygame.KEYDOWN, pygame.MOUSEBUTTONDOWN,pygame.QUIT, pygame.USEREVENT+2, pygame.USEREVENT+3, pygame.USEREVENT+4,  pygame.USEREVENT+5, pygame.USEREVENT+6])
+	pygame.event.set_allowed([pygame.KEYDOWN, pygame.MOUSEBUTTONDOWN,pygame.QUIT])
 
-def event_loop():
+def event_loop(frame_count):
 	global right_click
 
 	right_click =  False
@@ -54,20 +43,40 @@ def event_loop():
 		elif event.type == pygame.MOUSEBUTTONDOWN:
 			if event.button == 1:
 				right_click = True
+		
+	if frame_count > 4*global_config.speed and frame_count < (total_number_of_frames - 8*global_config.speed):
+		custom_event_loop(frame_count)
 
-		if event.type == pygame.USEREVENT+1:
-			coins_module.create_coin()
-		if event.type == pygame.USEREVENT+2:
-			obstacles_module.create_tree_obstacle()
-		if event.type == pygame.USEREVENT+3:
-			obstacles_module.create_rock_n_bush()
-		if event.type == pygame.USEREVENT+4:
-			bird_module.create_bird()
-		if event.type == pygame.USEREVENT+5:
-			dynamic_obstacle_olaf.create_olaf()
-		if event.type == pygame.USEREVENT+6:
-			dynamic_obstacle_santa.create_santa()
-		if event.type == pygame.USEREVENT+7:
-			dynamic_obstacle_giftbox.create_gift()
+def custom_event_loop(frame_count):
+	"""
+	Generates coins, trees, bushes, rocks and birds
+	"""
+	# Event 1 - Generate coin once in every 0.5 seconds
+	if (frame_count/global_config.speed)%0.5 == 0:
+		coins_module.create_coin()
+
+	# Event 2 - Generate tree obstacles once in every 6 seconds
+	if (frame_count/global_config.speed)%6 == 0:
+		obstacles_module.create_tree_obstacle()
+
+	# Event 3 - Generate rock and bush obstacles once in every 7 seconds
+	if (frame_count/global_config.speed)%7 == 0:
+		obstacles_module.create_rock_n_bush()
+
+	# Event 4 - Generate bird obstacle once in every 8 seconds
+	if (frame_count/global_config.speed)%8 == 0:
+		bird_module.create_bird()
+	
+	# Event 5 - Generate Reindeer obstacles once in every 20 seconds
+	if (frame_count/global_config.speed)%20 == 0:
+		dynamic_obstacle_olaf.create_olaf()
+
+	# Event 6 - Generate Santa Claus once in every 28 seconds
+	if (frame_count/global_config.speed)%28 == 0:
+		dynamic_obstacle_santa.create_santa()
+
+	# Event 7 - Generate Gift box obstacle once in every 40 seconds
+	if (frame_count/global_config.speed)%40 == 0:
+		dynamic_obstacle_giftbox.create_gift()
 		
 
